79af2462f77494ceaf18c1b9627ff4ef9d571bef
Kooku <koomg9599@gmail.com>
2019-04-27 17:44:06 +0900

    리스트뷰 생성

--- demo/app/src/main/java/com/example/android/tflitecamerademo/DrawView.java
+++ demo/app/src/main/java/com/example/android/tflitecamerademo/DrawView.java
@@ -11,13 +11,13 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class DrawView extends View{
-    private static final int HEATMAPWIGHT = 96;
+    private static final int HEATMAPWIDTH = 96;
     private static final int HEATMAPHEIGHT = 96;
     private static final int NUMJOINT = 14;
     /** Max preview width that is guaranteed by Camera2 API */
-    private static final int MAX_PREVIEW_WIDTH = 1920;
+    private static final int MAX_PREVIEW_HEIGHT = 1500;
     /** Max preview height that is guaranteed by Camera2 API */
-    private static final int MAX_PREVIEW_HEIGHT = 1080;
+    private static final int MAX_PREVIEW_WIDTH = 1080;
     private static int count = 0;
     private static int count1 = 0;
 
@@ -45,7 +45,7 @@ public class DrawView extends View{
         myView = DrawView.this;
         ppaint = new Paint();
         ppaint.setStrokeWidth(7);
-        ppaint.setColor(Color.BLUE);
+        ppaint.setColor(Color.CYAN);
 
         for(int i = 0; i< NUMJOINT; i++){
             paint[i] = new Paint();
@@ -56,7 +56,7 @@ public class DrawView extends View{
         paint[1].setColor(Color.RED);
         paint[2].setColor(Color.GREEN);
         paint[3].setColor(Color.YELLOW);
-        paint[4].setColor(Color.CYAN);
+        paint[4].setColor(Color.BLUE);
         paint[5].setColor(Color.DKGRAY);
         paint[6].setColor(Color.LTGRAY);
         paint[7].setColor(Color.MAGENTA);
@@ -65,7 +65,7 @@ public class DrawView extends View{
         paint[10].setColor(Color.RED);
         paint[11].setColor(Color.GREEN);
         paint[12].setColor(Color.YELLOW);
-        paint[13].setColor(Color.CYAN);
+        paint[13].setColor(Color.BLUE);
 
         setFocusable(true);
     }
@@ -75,11 +75,7 @@ public class DrawView extends View{
         System.out.println("count Check, onDraw 호출");
 //        canvas.drawText("토끼",250, 250, ppaint);
         count++;
-        Log.d("Draw", "******************");
-        for(int i = 0; i < NUMJOINT; i++){
-            Log.d("check1",i + " : " + arr[i][0] + " " + arr[i][1]);
-            canvas.drawPoint(arr[i][1], arr[i][0], paint[i]);
-        }
+
         canvas.drawLine(arr[0][1],arr[0][0], arr[1][1], arr[1][0], ppaint);
         canvas.drawLine(arr[1][1],arr[1][0], arr[2][1], arr[2][0], ppaint);
         canvas.drawLine(arr[1][1],arr[1][0], arr[5][1], arr[5][0], ppaint);
@@ -103,13 +99,16 @@ public class DrawView extends View{
         canvas.drawLine(arr[11][1],arr[11][0], arr[12][1], arr[12][0], ppaint);
         canvas.drawLine(arr[12][1],arr[12][0], arr[13][1], arr[13][0], ppaint);
 
-        Log.d("Draw", "******************");
+        for(int i = 0; i < NUMJOINT; i++){
+            Log.d("check1",i + " : " + arr[i][0] + " " + arr[i][1]);
+            canvas.drawPoint(arr[i][1], arr[i][0], paint[i]);
+        }
     }
 
 
     public static void setArr(int index, int[] inputArr) {
-        arr[index][0] = (float)inputArr[0] / HEATMAPWIGHT * MAX_PREVIEW_WIDTH;
-        arr[index][1] = (float)inputArr[1] / HEATMAPHEIGHT * MAX_PREVIEW_HEIGHT;
+        arr[index][0] = (float)inputArr[0] / HEATMAPHEIGHT * MAX_PREVIEW_HEIGHT;
+        arr[index][1] = (float)inputArr[1] / HEATMAPWIDTH * MAX_PREVIEW_WIDTH;
 
         if (index == 13){
             Log.d("count Check", "count: " +count + ", count1: " +count1++);

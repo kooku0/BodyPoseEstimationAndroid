289072bfdde9f6aa33f679979e846a17a0f7fb1c
Kooku <koomg9599@gmail.com>
2019-05-05 19:12:21 +0900

    poselistActivity 추가

---
+++ demo/app/src/main/java/com/example/android/tflitecamerademo/activity/PoseListActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.tflitecamerademo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.tflitecamerademo.R;
+
+public class PoseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pose_list);
+    }
+}
